6461be671a82be86560178ff86576578fc1e2c36
idolevi235 <idolevi235@gmail.com>
2020-03-22 19:25:48 +0200

    firebase database works

--- app/src/main/java/com/example/arrangeme/Homepage.java
+++ app/src/main/java/com/example/arrangeme/Homepage.java
@@ -1,15 +1,50 @@
 package com.example.arrangeme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Homepage extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class Homepage extends AppCompatActivity{
+
+    private DatabaseReference mDatabase;
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        mButton = (Button) findViewById(R.id.test_button);
+
+
+
+        mButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (v==mButton){
+                    mDatabase = FirebaseDatabase.getInstance().getReference("users");
+                    User user = new User("test", "test");
+                    mDatabase.push().setValue(user);
+                    Toast toast = Toast. makeText(getApplicationContext(),"User added",Toast. LENGTH_SHORT);
+                    toast. show();
+
+                    //User user = new User("test!!", "test!!");
+                    //mDatabase.child("111").setValue(user);
+                }
+            }
+
+
+        }));
+
+
     }
+
+
 }
 //
