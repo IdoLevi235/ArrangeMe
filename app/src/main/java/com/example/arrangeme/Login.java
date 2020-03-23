07204c8eed8d2cb4f6ef3cbcf5b27dd97f690645
linoy18 <46220147+linoy18@users.noreply.github.com>
2020-03-23 20:51:01 +0200

    login screen + homepage is working

---
+++ app/src/main/java/com/example/arrangeme/Login.java
@@ -0,0 +1,58 @@
+package com.example.arrangeme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class Login extends AppCompatActivity {
+
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private ImageView avatar;
+    private Button testButton;
+    private DatabaseReference mDatabase;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        username = (EditText) findViewById(R.id.userNameText);
+        password = (EditText) findViewById(R.id.passwordText);
+        login = (Button) findViewById(R.id.loginBtn);
+        login.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == login) {
+                    validate(username.getText().toString(), password.getText().toString());
+                }
+            }
+        }));
+
+
+    }
+
+    private void validate(String username, String userpassword)
+    {
+        System.out.println(username+" "+userpassword);
+        if (username.equals("a") && userpassword.equals("a")){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/arrangeme/MainActivity.java
+++ app/src/main/java/com/example/arrangeme/MainActivity.java
@@ -1,99 +1,65 @@
 package com.example.arrangeme;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-
-    private EditText username;
-    private EditText password;
-    private Button login;
-    private TextView infoText;
-    private ImageView avatar;
-    private Button testButton;
+   Button loginBtn;
     private DatabaseReference mDatabase;
-
-
-
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
-        //assign the variables to the id in the XML
-        username = (EditText) findViewById(R.id.userNameText);
-        password = (EditText) findViewById(R.id.passwordText);
-        login = (Button) findViewById(R.id.loginBtn);
-        avatar = (ImageView) findViewById(R.id.avatar);
-        infoText = (TextView) findViewById(R.id.infoText) ;
-        infoText.setText("Number of attempts remaining : 5");
-        //mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
-        login.setOnClickListener((new View.OnClickListener() {
+        loginBtn = (Button) findViewById(R.id.loginBtnMain);
+        mButton = (Button) findViewById(R.id.test_button);
+
+
+
+        mButton.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v == login) {
-                    validate(username.getText().toString(), password.getText().toString());
+                if (v==mButton){
+                    Log.d("db", "database activated");
+                    mDatabase = FirebaseDatabase.getInstance().getReference("users");
+                    User user = new User("a", "b");
+                    mDatabase.push().setValue(user);
+                    Toast toast = Toast. makeText(getApplicationContext(),"User added",Toast. LENGTH_SHORT);
+                    toast.show();
                 }
-            }
 
+            }
 
         }));
 
 
-    }
-
-    private void validate(String username, String userpassword)
-    {
-        System.out.println(username+" "+userpassword);
-        if (username.equals("a") && userpassword.equals("a")){
-            Intent intent = new Intent(MainActivity.this, Homepage.class);
-            startActivity(intent);
-        }
-    }
-
-   /* public void basicReadWrite() {
-        // [START write_message]
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
-        // [END write_message]
-        // [START read_message]
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        loginBtn.setOnClickListener((new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+            public void onClick(View v) {
+                if (v==loginBtn){
+                    Log.d("login", "login Btn Activated");
+                    Intent intent = new Intent(MainActivity.this, Login.class);
+                    startActivity(intent);
+                }
+
             }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-        // [END read_message]
-    }*/
-
+        }));
 
     }
+
+
+}
+//
