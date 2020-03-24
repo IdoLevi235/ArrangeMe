6dcc81d16c64e9908a5989d7922fc6ebcf55fd53
idolevi235 <idolevi235@gmail.com>
2020-03-24 13:24:59 +0200

    signup + authentication started

--- app/src/main/java/com/example/arrangeme/MainActivity.java
+++ app/src/main/java/com/example/arrangeme/MainActivity.java
@@ -10,24 +10,27 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity{
 
-   Button loginBtn;
+    Button loginBtn;
     private DatabaseReference mDatabase;
     Button mButton;
+    private FirebaseAuth mAuth;
+    Button signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         loginBtn = (Button) findViewById(R.id.loginBtnMain);
         mButton = (Button) findViewById(R.id.test_button);
-
-
-
+        mAuth = FirebaseAuth.getInstance(); //Firebase Authentication instanc
+        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+//TEST DB BUTTON onClick
         mButton.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,20 +47,60 @@ public class MainActivity extends AppCompatActivity{
 
         }));
 
-
-        loginBtn.setOnClickListener((new View.OnClickListener() {
+//Login button onClick
+        loginBtn.setOnClickListener((new View.OnClickListener() { //TODO: implement view.setonclick and make the code look nicer
             @Override
             public void onClick(View v) {
                 if (v==loginBtn){
                     Log.d("login", "login Btn Activated");
-                    Intent intent = new Intent(MainActivity.this, Login.class);
+                    Intent intent = new Intent(MainActivity.this, Login.class);//
                     startActivity(intent);
                 }
 
             }
+    }));
 
+//Sign Up button clicked
+        signUpBtn.setOnClickListener((new View.OnClickListener() { //TODO: implement view.setonclick and make the code look nicer
+            @Override
+            public void onClick(View v) {
+                if (v==signUpBtn){
+                    Log.d("signup", "signup Btn Activated");
+                    Intent intent = new Intent(MainActivity.this, Signup.class);//
+                    startActivity(intent);
+                }
+
+            }
         }));
 
+    }
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);//TODO: what happens in UI when user opens mainactivity
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
 
