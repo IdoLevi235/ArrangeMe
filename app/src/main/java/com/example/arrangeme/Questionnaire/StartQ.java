59f53ee9fe07b16f9d9cc0b878c71cdc218aa89c
linoy18 <46220147+linoy18@users.noreply.github.com>
2020-03-29 02:50:38 +0300

    Works

--- app/src/main/java/com/example/arrangeme/Questionnaire/StartQ.java
+++ app/src/main/java/com/example/arrangeme/Questionnaire/StartQ.java
@@ -1,5 +1,7 @@
 package com.example.arrangeme.Questionnaire;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,42 +11,100 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.*;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.arrangeme.R;
+
+import org.w3c.dom.Text;
+
+import java.util.function.ToDoubleBiFunction;
+
+import static com.example.arrangeme.R.*;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StartQ extends Fragment {
+public class StartQ extends Fragment  implements View.OnClickListener {
+    private Button femaleRec;
+    private ImageView female;
+
+    private Button maleRec;
+    private ImageView male;
+
+    private Button biSex;
+
+    private Button[] btn = new Button[3];
+    private Button btn_unfocus;
+    private int[] btn_id = {R.id.femaleRec, R.id.maleRec, R.id.biSex};
 
     public StartQ() {
         // Required empty public constructor
     }
+
     @Override
+    //Initializes the layout but not the attributes from the XML
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_q, container, false);
+        return inflater.inflate(layout.fragment_start_q, container, false);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final NavController navController = Navigation.findNavController(view);
-        Button button = view.findViewById(R.id.continue1);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v){
-                navController.navigate(R.id.action_startQ_to_screen2Q);
-            }
-        });
+        for(int i = 0; i < btn.length; i++){
+            btn[i] = (Button)getView().findViewById(btn_id[i]);
+            btn[i].setOnClickListener(this);
+        }
 
+        btn_unfocus = btn[0];
+        Button continue1 = view.findViewById(id.continue1);
+        continue1.setOnClickListener(this);
     }
+
+    @SuppressLint("ResourceAsColor")
+    public void onClick(View v){ //check for what button is pressed
+        final NavController navController = Navigation.findNavController(v);
+        ImageView male = (ImageView) getView().findViewById(id.imageViewMale);
+        ImageView female = (ImageView) getView().findViewById(id.imageViewFemale);
+
+        switch (v.getId()) {
+            case id.femaleRec:
+                setFocus(btn_unfocus,btn[0]);
+                //v.setBackgroundColor(R.color.colorProjectBlue);
+                //femaleT.setTextColor(Color.parseColor("#FFFFFF"));
+                break;
+            case id.maleRec:
+                setFocus(btn_unfocus, btn[1]);
+                //v.setBackgroundColor(R.color.colorProjectBlue);
+                //maleT.setTextColor(Color.parseColor("#FFFFFF"));
+                break;
+            case id.biSex:
+                setFocus(btn_unfocus, btn[2]);
+                //v.setBackgroundColor(R.color.colorProjectBlue);
+                //biSexT.setTextColor(Color.parseColor("#FFFFFF"));
+                break;
+           case id.continue1:
+                navController.navigate(id.action_startQ_to_screen2Q);
+            break;
+            default:
+                break;
+        }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void setFocus(Button btn_unfocus, Button btn_focus){
+        btn_unfocus.setTextColor(Color.parseColor("#000000"));
+        btn_unfocus.setBackgroundResource(R.drawable.rounded_rec_white);
+        btn_focus.setTextColor(Color.parseColor("#FFFFFF"));
+        btn_focus.setBackgroundResource(drawable.rounded_rec_blue);
+        this.btn_unfocus = btn_focus;
+    }
+
 }
