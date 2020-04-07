a8dbbc51cca833b8bbcd6a88c6618582586801a5
idolevi235 <idolevi235@gmail.com>
2020-04-07 23:28:04 +0300

    Identify if questionnaire is complete or not -  DONE.

--- app/src/main/java/com/example/arrangeme/Server.java
+++ app/src/main/java/com/example/arrangeme/Server.java
@@ -1,26 +1,13 @@
 package com.example.arrangeme;
 
+
 import android.app.Activity;
-import android.content.Intent;
-import android.util.Log;
-import android.widget.Button;
 
-import androidx.annotation.NonNull;
-
-import com.example.arrangeme.Questionnaire.Questionnaire;
-import com.example.arrangeme.Questionnaire.Screen19Q;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-
-import cn.pedant.SweetAlert.SweetAlertDialog;
 
 
-public class Server extends Activity {
+public class Server {
     public boolean isDone;
 
 
@@ -29,9 +16,6 @@ public class Server extends Activity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.child("users").child(Globals.UID).child("personality_vector").child(q).setValue(ans);
 
-    }
-
-    public static void isQuestionnaireFilled() {
     }
 
 
