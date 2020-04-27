193ba11c0041f3fa86d937e60501b5f893754188
idolevi235 <idolevi235@gmail.com>
2020-04-27 16:03:19 +0300

    tasks tab continue

--- app/src/main/java/com/example/arrangeme/ui/tasks/TaskPagePopup.java
+++ app/src/main/java/com/example/arrangeme/ui/tasks/TaskPagePopup.java
@@ -1,8 +1,16 @@
 package com.example.arrangeme.ui.tasks;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.PopupWindow;
 
 import androidx.annotation.Nullable;
 
@@ -12,15 +20,20 @@ public class TaskPagePopup extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.taskpagepopup);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-
         getWindow().setLayout((int) (width*0.7), (int) (height*0.5));
-
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity= Gravity.CENTER;
+        params.x=0;
+        params.y=-20;
+        getWindow().setAttributes(params);
+        //TODO: FADE FROM THE CENTER
 
     }
 }
+
+
+
