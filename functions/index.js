2d25f1086baffe69b1be2e080a0caf4a07a27838
linoy18 <46220147+linoy18@users.noreply.github.com>
2020-05-18 19:52:28 +0300

    kmeans works

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,11 @@ admin.initializeApp();
 
 // this function gets all the PVs from the database using firebase cloud functions
 exports.initKmeans = functions.https.onCall((data, context) => {
-var ref = admin.database().ref("simulated_users");
-    return ref.once("value" , function(snap){
+    var ref = admin.database().ref("simulated_users");
+    return ref.once("value", function (snap) {
         let vecs = [];
         let users = []
-    
+
         snap.forEach(x => {
             users.push(x.key);
             let arr = x.val().personality_vector;
@@ -30,5 +30,23 @@ var ref = admin.database().ref("simulated_users");
             updates['/simulated_users/' + users[i]] = postData;
             admin.database().ref().update(updates);
         }
+    });
+});
+
+// this function classify each new user to his sub group
+exports.classifyUser = functions.https.onCall((data, context) => {
+    admin.database().ref('users/' + data.id).once('value', (snap) => {
+        var pv = snap.val().personality_vector;
+        var results = [];
+        pv.shift();
+        admin.database().ref('details/kmeans/').once('value', (snap) => {
+            snap.forEach(x => {
+                results.push(euc(x.val(), pv));
+            })
+            var min_index = results.indexOf(Math.min(...results));
+            admin.database().ref('users/' + data.id).update({
+                group: min_index
+            });
+        });
     });
 });
