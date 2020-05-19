994da2c07d20bcdfc8603d345e1e1d90379600cd
idolevi235 <idolevi235@gmail.com>
2020-05-19 15:23:54 +0300

    Simulation start

--- app/src/main/java/com/example/arrangeme/Adminzone.java
+++ app/src/main/java/com/example/arrangeme/Adminzone.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.arrangeme.Enums.TaskCategory;
 import com.google.firebase.functions.FirebaseFunctions;
 import com.google.firebase.functions.HttpsCallableResult;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -18,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
@@ -42,6 +45,7 @@ public class Adminzone extends AppCompatActivity implements View.OnClickListener
         deleteSim1=(Button)findViewById(R.id.dSim1);
         kmeansBtn =(Button)findViewById(R.id.kmeansBtn);
         kmeansBtn.setOnClickListener(this);
+        deleteDB.setEnabled(false);
         deleteDB.setOnClickListener(this);
         sim1.setOnClickListener(this);
         deleteSim1.setOnClickListener(this);
@@ -107,14 +111,47 @@ public class Adminzone extends AppCompatActivity implements View.OnClickListener
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void simulate1000withPVnoSC() {
-        mDatabase = FirebaseDatabase.getInstance().getReference("simulated_users");
-        for(int i = 1 ; i<=1000 ; i++){
+        for(int i = 1 ; i<=2 ; i++){ //1000 users
             String s = Integer.toString(i);
+            mDatabase = FirebaseDatabase.getInstance().getReference("simulated_users").child("Sim" + s);
             HashMap<String,Integer> pv = calculate_PV();
-            mDatabase.child("Sim" + s).child("personality_vector").setValue(pv);
+            mDatabase.child("personality_vector").setValue(pv);
+            for (int j = 1 ; j<=10 ; j++){ //10 schedueles per each
+                String date = (j+10) + "-11-2022";
+                createRandomSchedule(date);
+            }
         }
 
     }
+
+    private void createRandomSchedule(String date) {
+        int key = 0;
+        ArrayList<String> hours = new ArrayList<String>()
+        {{
+            add("06:00");add("06:30");add("07:00");add("07:30");add("08:00");add("08:30");
+            add("09:00");add("09:30");add("10:00");add("10:30");add("11:00");add("11:30");
+            add("12:00");add("12:30");add("13:00");add("13:30");add("14:00");add("14:30");
+            add("15:00");add("15:30");add("16:00");add("16:30");add("17:00");add("17:30");
+            add("18:00");add("18:30");add("19:00");add("19:30");add("20:00");add("20:30");
+            add("21:00");add("21:30");add("22:00");add("22:30");add("23:00");add("23:30");
+            add("00:00");
+        }};
+
+        for (key=0;hours.size()>2;key++) { //the loop continues as long as there are at least 1 free hours in the "bank"
+            int hoursSize = hours.size();
+            mDatabase.child("Schedules").child(date).child("tempschedule").child(String.valueOf(key)).child("category")
+                    .setValue(TaskCategory.fromInt(ThreadLocalRandom.current().nextInt(0, 8 + 1))); // set random category
+            int randStartTimeIndex = (ThreadLocalRandom.current().nextInt(0,(hoursSize-1))); // get random start time
+            mDatabase.child("Schedules").child(date).child("tempschedule").child(String.valueOf(key)).child("startTime") // write random start time to DB
+                    .setValue(hours.get(randStartTimeIndex));
+            int randEndTimeIndex = (ThreadLocalRandom.current().nextInt(randStartTimeIndex+1,(hoursSize-1) + 1)); // get random start time
+            mDatabase.child("Schedules").child(date).child("tempschedule").child(String.valueOf(key)).child("endTime")
+                    .setValue(hours.get(randEndTimeIndex));
+                    hours.subList(randStartTimeIndex,randEndTimeIndex+1).clear();
+
+        }
+        }
+
 
     /**
      * Method calculate_PV- calculating the personality vector to each simulated user.
--- app/src/main/java/com/example/arrangeme/Enums/TaskCategory.java
+++ app/src/main/java/com/example/arrangeme/Enums/TaskCategory.java
@@ -1,80 +1,80 @@
 package com.example.arrangeme.Enums;
 
 public enum TaskCategory {
-    STUDY, SPORT, WORK, NUTRITION, FAMILY, CHORES, RELAX, FRIENDS, OTHER;
+  STUDY, SPORT, WORK, NUTRITION, FAMILY, CHORES, RELAX, FRIENDS, OTHER;
 
-    public static TaskCategory fromInt (int x){
-        TaskCategory t;
-        switch (x){
-            case 0:
-                t=STUDY;
-                break;
-            case 1:
-                t=SPORT;
-                break;
-            case 2:
-                t=WORK;
-                break;
-            case 3:
-                t=NUTRITION;
-                break;
-            case 4:
-                t=FAMILY;
-                break;
-            case 5:
-                t=CHORES;
-                break;
-            case 6:
-                t=RELAX;
-                break;
-            case 7:
-                t=FRIENDS;
-                break;
-            case 8:
-                t=OTHER;
-                break;
-            default:
-                t=null;
-                break;
-        }
-        return t;
+  public static TaskCategory fromInt (int x){
+    TaskCategory t;
+    switch (x){
+      case 0:
+        t=STUDY;
+        break;
+      case 1:
+        t=SPORT;
+        break;
+      case 2:
+        t=WORK;
+        break;
+      case 3:
+        t=NUTRITION;
+        break;
+      case 4:
+        t=FAMILY;
+        break;
+      case 5:
+        t=CHORES;
+        break;
+      case 6:
+        t=RELAX;
+        break;
+      case 7:
+        t=FRIENDS;
+        break;
+      case 8:
+        t=OTHER;
+        break;
+      default:
+        t=null;
+        break;
     }
+    return t;
+  }
 
-    public static int fromStringToInt (String s){
-        int t;
-        switch (s){
-            case "STUDY":
-                t=0;
-                break;
-            case "SPORT":
-                t=1;
-                break;
-            case "WORK":
-                t=2;
-                break;
-            case "NUTRITION":
-                t=3;
-                break;
-            case "FAMILY":
-                t=4;
-                break;
-            case "CHORES":
-                t=5;
-                break;
-            case "RELAX":
-                t=6;
-                break;
-            case "FRIENDS":
-                t=7;
-                break;
-            case "OTHER":
-                t=8;
-                break;
-            default:
-                t=-1;
-                break;
-        }
-        return t;
+  public static int fromStringToInt (String s){
+    int t;
+    switch (s){
+      case "STUDY":
+        t=0;
+        break;
+      case "SPORT":
+        t=1;
+        break;
+      case "WORK":
+        t=2;
+        break;
+      case "NUTRITION":
+        t=3;
+        break;
+      case "FAMILY":
+        t=4;
+        break;
+      case "CHORES":
+        t=5;
+        break;
+      case "RELAX":
+        t=6;
+        break;
+      case "FRIENDS":
+        t=7;
+        break;
+      case "OTHER":
+        t=8;
+        break;
+      default:
+        t=-1;
+        break;
     }
+    return t;
+  }
 
 }
